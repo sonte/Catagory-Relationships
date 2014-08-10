6403dc8d31aba23bb72c4ccbf2cd66e791618402
sonte <matthewstone96@gmail.com>
2014-08-10 17:09:28 -0500

    Fixed issue
    The initial problem states that if a item has a parent it cannot be a
    root. Added in a check for the root object in case it has a parent to
    ignore it as a root object.

--- sorter.py
+++ sorter.py
@@ -2,20 +2,20 @@ from xml.dom import minidom
 import xml.etree.ElementTree as et
 
 __author__ = 'sonte'
-#creating our dictionaries
+# creating our dictionaries
 relationDictionary = dict()
 catDictionary = dict()
 #pulling in the xml and getting all the trees with the attribute 'category'
 doc = et.parse('categories.xml')
 child = doc.findall('category')
-x=0
+x = 0
 '''
 Under the Category tree in the xml assigning the text from the id to be the key
 and the name to be the value in the catDictionary
 '''
-while (x<len(child)):
+while (x < len(child)):
     catDictionary[int(child[x][0].text)] = child[x][1].text
-    x = x +1
+    x = x + 1
 
 #Parsing the xml from the other xml file and getting all the trees with the attribute 'category_relationship'
 doc = et.parse('category_relationships.xml')
@@ -41,33 +41,36 @@ for key in relationDictionary:
     First it checks to make sure that the key's value does not have multiple entries.  If it does we know that
     it is not going to be our starting point, so skip to the next key.
     '''
-    if (len(relationDictionary[key])==1):
+    if (len(relationDictionary[key]) == 1):
         '''
         Next it checks to make sure that the key's value exists in the catDictionary.  If it does not, then there
         is no corresponding name in the categories.xml files and should be skipped.
         '''
-        if (catDictionary.get(key) != None ):
-            #Now that we have gotten to this point, we know that the key is the first word in the catDictionary.
-            first = catDictionary.get(key)
-            '''
-            We know that they value of the key is the second word, but this is a list so we have to pull it out to convert it
-            because none of the keys are lists in catDictionary. list[0] is just and int which is what the keys are in
-            catDictionary
-            '''
-            list = relationDictionary.get(key)
-            second = catDictionary.get(list[0])
-            '''
-            We know that the second word is going to be followed by the third and final word.  We use the second word as the key and
-            get its list of values and the different possible third words.
-            '''
-            list = relationDictionary.get(list[0])
-            #go through all of the items in list to get all of the possible third word combinations.
-            for items in list:
-                #check to make the third word exits of course.
-                if (catDictionary.get(items) != None):
-                    third = catDictionary.get(items)
-                    print(first + " " + second + " " + third)
-    x = x +1
+        findkey = relationDictionary.get(key)
+        list = relationDictionary.get(findkey[0])
+        if key not in list:
+            if (catDictionary.get(key) != None ):
+                #Now that we have gotten to this point, we know that the key is the first word in the catDictionary.
+                first = catDictionary.get(key)
+                '''
+                We know that they value of the key is the second word, but this is a list so we have to pull it out to convert it
+                because none of the keys are lists in catDictionary. list[0] is just and int which is what the keys are in
+                catDictionary
+                '''
+                list = relationDictionary.get(key)
+                second = catDictionary.get(list[0])
+                '''
+                We know that the second word is going to be followed by the third and final word.  We use the second word as the key and
+                get its list of values and the different possible third words.
+                '''
+                list = relationDictionary.get(list[0])
+                #go through all of the items in list to get all of the possible third word combinations.
+                for items in list:
+                    #check to make the third word exits of course.
+                    if (catDictionary.get(items) != None):
+                        third = catDictionary.get(items)
+                        print(first + " > " + second + " > " + third)
+    x = x + 1
 
 
 
